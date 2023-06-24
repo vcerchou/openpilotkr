3537dedb79740c006883ed8d0c858e9401a2b0e2
multikyd <multikyd@gmail.com>
2023-06-25 01:02:21 +0900

    opkr

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -3,21 +3,21 @@
 
 // include the safety policies.
 #include "safety/safety_defaults.h"
-//#include "safety/safety_honda.h"
-//#include "safety/safety_toyota.h"
-//#include "safety/safety_tesla.h"
-//#include "safety/safety_gm.h"
-//#include "safety/safety_ford.h"
+#include "safety/safety_honda.h"
+#include "safety/safety_toyota.h"
+#include "safety/safety_tesla.h"
+#include "safety/safety_gm.h"
+#include "safety/safety_ford.h"
 #include "safety/safety_hyundai.h"
-//#include "safety/safety_chrysler.h"
-//#include "safety/safety_subaru.h"
-//#include "safety/safety_subaru_legacy.h"
-//#include "safety/safety_mazda.h"
-//#include "safety/safety_nissan.h"
-//#include "safety/safety_volkswagen_mqb.h"
-//#include "safety/safety_volkswagen_pq.h"
+#include "safety/safety_chrysler.h"
+#include "safety/safety_subaru.h"
+#include "safety/safety_subaru_preglobal.h"
+#include "safety/safety_mazda.h"
+#include "safety/safety_nissan.h"
+#include "safety/safety_volkswagen_mqb.h"
+#include "safety/safety_volkswagen_pq.h"
 #include "safety/safety_elm327.h"
-//#include "safety/safety_body.h"
+#include "safety/safety_body.h"
 
 // CAN-FD only safety modes
 #ifdef CANFD
@@ -293,27 +293,28 @@ typedef struct {
 
 const safety_hook_config safety_hook_registry[] = {
   {SAFETY_SILENT, &nooutput_hooks},
-  //{SAFETY_HONDA_NIDEC, &honda_nidec_hooks},
-  //{SAFETY_TOYOTA, &toyota_hooks},
+  {SAFETY_HONDA_NIDEC, &honda_nidec_hooks},
+  {SAFETY_TOYOTA, &toyota_hooks},
   {SAFETY_ELM327, &elm327_hooks},
-  //{SAFETY_GM, &gm_hooks},
-  //{SAFETY_HONDA_BOSCH, &honda_bosch_hooks},
+  {SAFETY_GM, &gm_hooks},
+  {SAFETY_HONDA_BOSCH, &honda_bosch_hooks},
   {SAFETY_HYUNDAI, &hyundai_hooks},
-  //{SAFETY_CHRYSLER, &chrysler_hooks},
-  //{SAFETY_SUBARU, &subaru_hooks},
-  //{SAFETY_VOLKSWAGEN_MQB, &volkswagen_mqb_hooks},
-  //{SAFETY_NISSAN, &nissan_hooks},
+  {SAFETY_CHRYSLER, &chrysler_hooks},
+  {SAFETY_SUBARU, &subaru_hooks},
+  {SAFETY_VOLKSWAGEN_MQB, &volkswagen_mqb_hooks},
+  {SAFETY_NISSAN, &nissan_hooks},
   {SAFETY_NOOUTPUT, &nooutput_hooks},
   {SAFETY_HYUNDAI_LEGACY, &hyundai_legacy_hooks},
-  //{SAFETY_MAZDA, &mazda_hooks},
-  //{SAFETY_BODY, &body_hooks},
+  {SAFETY_MAZDA, &mazda_hooks},
+  {SAFETY_BODY, &body_hooks},
+  {SAFETY_FORD, &ford_hooks},
 #ifdef CANFD
   {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
 #endif
 #ifdef ALLOW_DEBUG
-  //{SAFETY_TESLA, &tesla_hooks},
-  //{SAFETY_SUBARU_LEGACY, &subaru_legacy_hooks},
-  //{SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
+  {SAFETY_TESLA, &tesla_hooks},
+  {SAFETY_SUBARU_PREGLOBAL, &subaru_preglobal_hooks},
+  {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
   {SAFETY_ALLOUTPUT, &alloutput_hooks},
 #endif
 };

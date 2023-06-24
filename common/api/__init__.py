87c28945e35b98665e26cda39f6a70de7708fa92
multikyd <multikyd@gmail.com>
2023-06-25 01:15:25 +0900

    Revert "Revert "opkr""
    This reverts commit d21038391958eb2d872aba0edef9d4edc81a2406.

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from common.basedir import PERSIST
 from system.version import get_version
 
-API_HOST = os.getenv('API_HOST', 'http://opkr.o-kr.kr:3000')
+API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 
 class Api():
   def __init__(self, dongle_id):
--- selfdrive/ui/qt/maps/map_helpers.h
+++ selfdrive/ui/qt/maps/map_helpers.h
@@ -10,9 +10,11 @@
 #include "common/transformations/orientation.hpp"
 #include "cereal/messaging/messaging.h"
 
-const QString MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
-//const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
-const QString MAPS_HOST = util::getenv("MAPS_HOST", "https://api.mapbox.com").c_str();
+#include "common/params.h"
+
+//const QString MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
+const QString MAPBOX_TOKEN = QString::fromStdString(Params().get("MapboxToken"));
+const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
 const QString MAPS_CACHE_PATH = "/data/mbgl-cache-navd.db";
 
 QString get_mapbox_token();

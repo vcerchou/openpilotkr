3d5b577bd7087c010fab53381f7bebeb05fb8763
multikyd <72329880+multikyd@users.noreply.github.com>
2023-07-11 14:46:51 +0900

    opkr

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1270,44 +1270,32 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     if (rpm > 1) {
       startAngle = 225 * 16;
       spanAngle = int(fmax(-45, (-15*count))) * 16;
-      QPen pen1(QBrush(QColor(25, 127, 54, 200)), 55);
-      pen1.setCapStyle(Qt::FlatCap);
-      p.setPen(pen1);
+      p.setPen(QPen(QBrush(QColor(25, 127, 54, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
 
       startAngle = 180 * 16;
       spanAngle = int(fmax(-45, -15*(fmax(0, count-3)))) * 16;
-      QPen pen2(QBrush(QColor(34, 177, 76, 200)), 55);
-      pen2.setCapStyle(Qt::FlatCap);
-      p.setPen(pen2);
+      p.setPen(QPen(QBrush(QColor(34, 177, 76, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
 
       startAngle = 135 * 16;
       spanAngle = int(fmax(-45, -15*(fmax(0, count-6)))) * 16;
-      QPen pen3(QBrush(QColor(0, 255, 0, 200)), 55);
-      pen3.setCapStyle(Qt::FlatCap);
-      p.setPen(pen3);
+      p.setPen(QPen(QBrush(QColor(0, 255, 0, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
 
       startAngle = 90 * 16;
       spanAngle = int(fmax(-45, -15*(fmax(0, count-9)))) * 16;
-      QPen pen4(QBrush(QColor(255, 201, 14, 200)), 55);
-      pen4.setCapStyle(Qt::FlatCap);
-      p.setPen(pen4);
+      p.setPen(QPen(QBrush(QColor(255, 201, 14, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
 
       startAngle = 45 * 16;
       spanAngle = int(fmax(-45, -15*(fmax(0, count-12)))) * 16;
-      QPen pen5(QBrush(QColor(255, 127, 39, 200)), 55);
-      pen5.setCapStyle(Qt::FlatCap);
-      p.setPen(pen5);
+      p.setPen(QPen(QBrush(QColor(255, 127, 39, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
 
       startAngle = 0 * 16;
       spanAngle = int(fmax(-45, -15*(fmax(0, count-15)))) * 16;
-      QPen pen6(QBrush(QColor(255, 0, 0, 200)), 55);
-      pen6.setCapStyle(Qt::FlatCap);
-      p.setPen(pen6);
+      p.setPen(QPen(QBrush(QColor(255, 0, 0, 200)),55,Qt::SolidLine,Qt::FlatCap));
       p.drawArc(rectangle, startAngle, spanAngle);
     }
   }

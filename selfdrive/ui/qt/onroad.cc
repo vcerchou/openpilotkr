a9c374e847b8a67a4c1c7dec81559edf9bcd40d4
multikyd <72329880+multikyd@users.noreply.github.com>
2023-07-13 09:49:12 +0900

    opkr

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -65,6 +65,7 @@ void OnroadWindow::updateState(const UIState &s) {
       QDateTime modifiedtime = fileInfo.lastModified();
       QString modified_time = modifiedtime.toString("yyyy-MM-dd hh:mm:ss ");
       const std::string txt = util::read_file("/data/log/error.txt");
+      printf("%s", txt.c_str());
       if (RichTextDialog::alert(modified_time + QString::fromStdString(txt), this)) {
         uiState()->scene.tmux_error_check = true;
       }

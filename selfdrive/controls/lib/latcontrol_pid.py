4c0d520d0712c1178e4b85179a1a801d76338116
multikyd <72329880+multikyd@users.noreply.github.com>
2023-07-13 19:18:59 +0900

    Revert "opkr"
    This reverts commit 1f8ce928650f3700e7a4646ed98b44765b96b814.

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -12,46 +12,14 @@ class LatControlPID(LatControl):
     super().__init__(CP, CI)
     self.pid = PIDController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                              (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
-                             k_f=CP.lateralTuning.pid.kf,
-                             (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
-                             pos_limit=self.steer_max, neg_limit=-self.steer_max)
+                             k_f=CP.lateralTuning.pid.kf, pos_limit=self.steer_max, neg_limit=-self.steer_max)
     self.get_steer_feedforward = CI.get_steer_feedforward_function()
-
-    self.mpc_frame = 0
-    self.params = Params()
-
-    self.live_tune_enabled = False
-
-    self.lp_timer = 0
 
   def reset(self):
     super().reset()
     self.pid.reset()
 
-  # live tune referred to kegman's 
-  def live_tune(self):
-    self.mpc_frame += 1
-    if self.mpc_frame % 300 == 0:
-      self.steerKpV = float(Decimal(self.params.get("PidKp", encoding="utf8")) * Decimal('0.01'))
-      self.steerKiV = float(Decimal(self.params.get("PidKi", encoding="utf8")) * Decimal('0.001'))
-      self.steerKf = float(Decimal(self.params.get("PidKf", encoding="utf8")) * Decimal('0.00001'))
-      self.steerKdV = float(Decimal(self.params.get("PidKd", encoding="utf8")) * Decimal('0.01'))
-      self.pid = PIDController(([0., 9.], [0.1, self.steerKpV]),
-                          ([0., 9.], [0.01, self.steerKiV]),
-                          k_f=self.steerKf,
-                          ([0.], [self.steerKdV]),
-                          pos_limit=self.steer_max, neg_limit=-self.steer_max)
-      self.mpc_frame = 0
-
-
   def update(self, active, CS, VM, params, last_actuators, steer_limited, desired_curvature, desired_curvature_rate, llk):
-    self.lp_timer += 1
-    if self.lp_timer > 100:
-      self.lp_timer = 0
-      self.live_tune_enabled = self.params.get_bool("OpkrLiveTunePanelEnable")
-    if self.live_tune_enabled:
-      self.live_tune()
-
     pid_log = log.ControlsState.LateralPIDState.new_message()
     pid_log.steeringAngleDeg = float(CS.steeringAngleDeg)
     pid_log.steeringRateDeg = float(CS.steeringRateDeg)

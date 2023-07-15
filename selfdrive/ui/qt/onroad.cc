55ee897b7f7228762a297d033506e41b85437ae0
multikyd <multikyd@gmail.com>
2023-07-16 00:22:58 +0900

    opkr

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1274,6 +1274,16 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     }
   }
 
+  if (s->scene->cal_view) {
+    p.setPen(QPen(Qt::white, 3));
+    for (int i = 0; i < 5; i++) {
+      p.drawLine(0, (i*108), s->fb_w, (i*108))
+    }
+    for (int i = 0; i < 8; i++) {
+      p.drawLine((i*120), 0, (i*120), s->fb_h)
+    }
+  }
+
   p.restore();
 }
 

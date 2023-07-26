ee919c01a38dd7fd50d4181d73ae26d9b2a08454
multikyd <72329880+multikyd@users.noreply.github.com>
2023-07-26 22:46:03 +0900

    opkr_nak

--- selfdrive/ui/qt/maps/map_helpers.h
+++ selfdrive/ui/qt/maps/map_helpers.h
@@ -10,16 +10,7 @@
 #include "common/transformations/orientation.hpp"
 #include "cereal/messaging/messaging.h"
 
-#include "common/params.h"
-
-
-QString MAPBOX_TOKEN = "";
-if (QString::fromStdString(Params().get("MapboxToken")).isEmpty()) {
-  MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
-} else {
-  MAPBOX_TOKEN = QString::fromStdString(Params().get("MapboxToken"));
-}	
-
+const QString MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
 const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
 const QString MAPS_CACHE_PATH = "/data/mbgl-cache-navd.db";
 

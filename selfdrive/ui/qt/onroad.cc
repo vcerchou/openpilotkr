f1640e596d0573fb9029bc99e33334f34ba47788
multikyd <multikyd@gmail.com>
2023-07-28 01:24:50 +0900

    opkr_ack

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -94,7 +94,7 @@ void OnroadWindow::updateState(const UIState &s) {
 void OnroadWindow::mousePressEvent(QMouseEvent* e) {
 
   QRect stockui_btn = QRect(15, uiState()->scene.low_ui_profile?693:15, 184, 202);
-  QRect tuneui_btn = QRect(uiState()->scene.mapbox_enabled?1600:1960, uiState()->scene.low_ui_profile?895:15, 170, 170);
+  QRect tuneui_btn = QRect(uiState()->scene.mapbox_enabled?1420:1960, uiState()->scene.low_ui_profile?895:15, 170, 170);
   QRect speedlimit_btn = QRect(220, uiState()->scene.low_ui_profile?700:15, 190, 190);
   QRect monitoring_btn = QRect(20, uiState()->scene.low_ui_profile?20:860, 190, 190);
   QRect multi_btn = QRect(1960, uiState()->scene.low_ui_profile?15:895, 160, 160);
@@ -1818,7 +1818,7 @@ void AnnotatedCameraWidget::paintGL() {
     drawDriverState(painter, s);
   }
 
-  if (!s->scene.mapbox_enabled) {
+  if (!s->scene.mapbox_enabled || s->scene.mapbox_running) {
     drawWheelState(painter, s);
   }
 

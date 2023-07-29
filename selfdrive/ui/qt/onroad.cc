6ad2f5672e5df1eb018590a40f1aab86289da9ec
multikyd <multikyd@gmail.com>
2023-07-30 00:54:58 +0900

    opkr_ack

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -63,13 +63,10 @@ OnroadWindow::OnroadWindow(QWidget *parent) : QWidget(parent) {
     }
   });
 	record_timer->start(1000/UI_FREQ);
-  QWidget* recorder_widget = new QWidget(this);
-  QVBoxLayout * recorder_layout = new QVBoxLayout (recorder_widget);
-  recorder = new ScreenRecoder(this);
-  recorder_layout->addWidget(recorder);
 
-  stacked_layout->addWidget(recorder_widget);
-  recorder_widget->raise();
+  recorder = new ScreenRecoder(this);
+  stacked_layout->addWidget(recorder);
+  recorder->raise();
   alerts->raise();
 }
 

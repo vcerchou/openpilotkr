cad435a99a76342054dd46961d57fd79f8051388
multikyd <72329880+multikyd@users.noreply.github.com>
2023-07-29 17:46:50 +0900

    opkr_ack

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -641,8 +641,8 @@ class CarController:
               if self.switch_timer > 0:
                 self.switch_timer -= 1
               else:
-                can_sends.extend([hyundaican.create_clu11(self.packer, self.resume_cnt, CS.clu11, btn_signal)] * 5) if not self.longcontrol \
-                else can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, btn_signal, clu11_speed, self.CP.sccBus)] * 5)
+                can_sends.extend([hyundaican.create_clu11(self.packer, self.resume_cnt, CS.clu11, btn_signal)] * 4) if not self.longcontrol \
+                else can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, btn_signal, clu11_speed, self.CP.sccBus)] * 4)
                 self.resume_cnt += 1
                 if self.resume_cnt >= randint(6, 8):
                   self.resume_cnt = 0
@@ -737,8 +737,8 @@ class CarController:
               elif self.switch_timer > 0:
                 self.switch_timer -= 1
               else:
-                can_sends.extend([hyundaican.create_clu11(self.packer, self.resume_cnt, CS.clu11, btn_signal)] * 5) if not self.longcontrol \
-                else can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, btn_signal, clu11_speed, self.CP.sccBus)] * 5)
+                can_sends.extend([hyundaican.create_clu11(self.packer, self.resume_cnt, CS.clu11, btn_signal)] * 4) if not self.longcontrol \
+                else can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, btn_signal, clu11_speed, self.CP.sccBus)] * 4)
                 self.resume_cnt += 1
                 if self.resume_cnt >= randint(6, 8):
                   self.resume_cnt = 0

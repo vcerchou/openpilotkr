43bbaa5160edf31739a70531c5391ffed316d3a3
multikyd <multikyd@gmail.com>
2023-07-29 20:25:06 +0900

    opkr_ack

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -377,7 +377,7 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : ListWidget(parent) {
   lastUpdateLbl = new LabelControl(tr("Last Update Check"), "", "");
   updateBtn = new ButtonControl(tr("Check for Updates"), "");
   connect(updateBtn, &ButtonControl::clicked, [=]() {
-    std::system("/data/openpilot/selfdrive/assets/addon/script/gitcommit.sh &");
+    std::system("/data/openpilot/selfdrive/assets/addon/script/gitcommit.sh");
     std::system("date '+%F %T' > /data/params/d/LastUpdateTime");
     QString last_ping = QString::fromStdString(params.get("LastAthenaPingTime"));
     QString desc = "";

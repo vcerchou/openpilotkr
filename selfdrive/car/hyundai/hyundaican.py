bb39346baff0f7d6c1c6af1a861702a9c4df1ce0
multikyd <72329880+multikyd@users.noreply.github.com>
2023-08-05 17:16:03 +0900

    opkr_ack

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -25,7 +25,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     "CF_Lkas_LdwsOpt_USM",
   ]}
   values["CF_Lkas_LdwsSysState"] = sys_state
-  values["CF_Lkas_SysWarning"] = 3 # if (sys_warning and car_fingerprint not in LEGACY_SAFETY_MODE_CAR_ALT) else 0
+  values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0 # if (sys_warning and car_fingerprint not in LEGACY_SAFETY_MODE_CAR_ALT) else 0
   values["CF_Lkas_LdwsLHWarning"] = left_lane_depart
   values["CF_Lkas_LdwsRHWarning"] = right_lane_depart
   values["CR_Lkas_StrToqReq"] = apply_steer
